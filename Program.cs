dfa287eeffed2c25b5dc24fa0b055fe812322c7e
Erik Sabino <eriksabino@Eriks-MacBook-Air.local>
2025-05-12 09:46:01 -0300

    finalizar programa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography;
+
+int naipe = RandomNumberGenerator.GetInt32(1, 5);
+string nomeNaipe = naipe switch
+{
+    1 => "Copas",
+    2 => "Espadas",
+    3 => "Ouros",
+    4 => "Paus",
+    _ => ""
+};
+Console.WriteLine(nomeNaipe);
